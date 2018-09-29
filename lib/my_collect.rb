fe23cf98f905355bf94e5a0a140bb87f2ecd7adb
hmalik88 <hbmalik88@gmail.com>
2018-09-29 00:47:24 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+	if array.size > 0
+		counter = 0
+		modified_array = []
+		while counter < array.size
+			modified_array.push(yield array[counter])
+			counter += 1
+		end
+	end
+	modified_array
+end
